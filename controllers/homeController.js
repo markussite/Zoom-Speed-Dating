9537f9f1500be7ce38aa78f3c9f7bc7c373392a2
erirapce <hermesrapce@gmail.com>
2021-04-27 20:56:47 +0200

    create the app structure

---
+++ controllers/homeController.js
@@ -0,0 +1,9 @@
+exports.showCourses = (req, res) => {
+  res.render("courses");
+};
+exports.showSignUp = (req, res) => {
+  res.render("contact");
+};
+exports.postedSignUpForm = (req, res) => {
+  res.render("thanks");
+};
---
+++ main.js
@@ -0,0 +1,46 @@
+//Require the error handler controller.
+const errorController = require("./controllers/errorController")
+
+//Initialise the app
+const express = require("express"),
+  app = express();
+
+app.set("port",process.env.PORT || 3000);
+
+//Enables static assets
+app.use(express.static("public"))
+
+//Require the express-ejs-layouts module
+const layouts = require("express-ejs-layouts");
+app.set("view engine", "ejs"); // Set the application to use ejs.
+app.use(layouts); //Set the application to use the layout.
+
+//Tell the Express.js app to use body-parser for processing URL-encoded and JSON parameters
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Zoom Speed Dating app!");
+});
+
+const homeController = require("./controllers/homeController")
+
+//Add routes for the courses, page, contact page and contact form submission.
+app.get("/courses", homeController.showCourses);
+app.get("/contact", homeController.showSignUp);
+app.post("/contact", homeController.postedSignUpForm);
+//Add error handlers as middleware functions.
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
+
+app.listen(app.get("port"), () => {
+  console.log(
+    `Server running at http://localhost:${app.get(
+      "port"
+    )}`
+  );
+});
